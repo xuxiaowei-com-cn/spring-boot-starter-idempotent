514ba2213e61387bbd395f2786f131115bad98e7
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-02-23 21:55:56 +0800

    幂等注解

---
+++ src/main/java/cn/com/xuxiaowei/boot/idempotent/annotation/Idempotent.java
@@ -0,0 +1,62 @@
+package cn.com.xuxiaowei.boot.idempotent.annotation;
+
+import java.lang.annotation.*;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 幂等注解
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@Inherited
+@Target(ElementType.METHOD)
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface Idempotent {
+
+    /**
+     * 幂等 Token 在 header 中的名字
+     * <p>
+     * 第一优先级
+     *
+     * @return 返回幂等Token在header中的名字
+     */
+    String header() default "";
+
+    /**
+     * 幂等 Token 在 参数 中的名字
+     * <p>
+     * 第二优先级（当{@link #header}为空时）
+     *
+     * @return 返回幂等Token在参数中的名字
+     */
+    String param() default "";
+
+    /**
+     * 幂等放入Redis中的键值
+     * <p>
+     * 支持 Spring-EL 表达式
+     *
+     * @return 幂等键值
+     */
+    String key();
+
+    /**
+     * 有效期
+     * <p>
+     * 默认：1
+     *
+     * @return 有效期
+     */
+    int expireTime() default 1;
+
+    /**
+     * 有效期时间单位
+     * <p>
+     * 默认：s
+     *
+     * @return {@link TimeUnit}
+     */
+    TimeUnit timeUnit() default TimeUnit.SECONDS;
+
+}
