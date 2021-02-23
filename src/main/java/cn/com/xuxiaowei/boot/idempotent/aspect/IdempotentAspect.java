4e239614079ab6976c903d03f920b41d39ebcef2
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-02-23 22:48:13 +0800

    Redis

--- src/main/java/cn/com/xuxiaowei/boot/idempotent/aspect/IdempotentAspect.java
+++ src/main/java/cn/com/xuxiaowei/boot/idempotent/aspect/IdempotentAspect.java
@@ -1,6 +1,7 @@
 package cn.com.xuxiaowei.boot.idempotent.aspect;
 
 import cn.com.xuxiaowei.boot.idempotent.annotation.Idempotent;
+import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
@@ -8,6 +9,8 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.context.request.RequestAttributes;
@@ -16,7 +19,6 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
-import java.util.HashMap;
 
 /**
  * 幂等切面
@@ -28,6 +30,13 @@ import java.util.HashMap;
 @Aspect
 @Component
 public class IdempotentAspect {
+
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    public void setStringRedisTemplate(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
 
     /**
      * 切点
@@ -45,13 +54,20 @@ public class IdempotentAspect {
     @Around("pointcut()")
     public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
 
+        // 获取请求
         RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
         ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) requestAttributes;
 
         if (servletRequestAttributes == null) {
+            // 未获取到请求
+
             log.error("幂等切面前置通知未获取到 ServletRequestAttributes，幂等切面失效");
+
+            // 执行方法并返回结果
             return joinPoint.proceed();
         } else {
+
+            // 获取 Http 请求
             HttpServletRequest request = servletRequestAttributes.getRequest();
 
             // 获取幂等注解
@@ -60,33 +76,69 @@ public class IdempotentAspect {
             Method method = methodSignature.getMethod();
             Idempotent idempotent = method.getAnnotation(Idempotent.class);
 
+            // 获取幂等在请求头中的TokenName
             String header = idempotent.header();
+            // 获取幂等在请求参数中的TokenName
             String param = idempotent.param();
 
             if (StringUtils.hasText(header) || StringUtils.hasText(param)) {
+                // 存在请求头或参数中的TokenName
+
+                // 获取请求头中的TokenValue
                 String headerValue = request.getHeader(header);
+                // 获取请求参数中的TokenValue
                 String paramValue = request.getParameter(param);
+
                 if (StringUtils.hasText(headerValue)) {
-
-                    log.info(headerValue);
-
-                    return new HashMap<>();
+                    // 存在请求头中的TokenValue
+                    // 根据请求头中的TokenValue获取Redis中缓存的结果
+                    return getProceed(joinPoint, headerValue);
                 } else if (StringUtils.hasText(paramValue)) {
-
-                    log.info(paramValue);
-
-                    return new HashMap<>();
+                    // 根据参数中的TokenValue获取Redis中缓存的结果
+                    return getProceed(joinPoint, paramValue);
                 } else {
                     log.error("幂等从请求中未获取到Token，幂等失效");
+
+                    // 执行方法并返回结果
                     return joinPoint.proceed();
                 }
             } else {
+                // 不存在请求头或参数中的TokenName
+
                 log.error("幂等注解 header、param 均为空，幂等失效");
+
+                // 执行方法并返回结果
                 return joinPoint.proceed();
             }
 
         }
 
+    }
+
+    /**
+     * 获取 Redis 中的缓存结果
+     *
+     * @param joinPoint  切面方法信息
+     * @param tokenValue TokenValue
+     * @return 返回 Redis 中的缓存结果
+     * @throws Throwable 执行异常
+     */
+    private Object getProceed(ProceedingJoinPoint joinPoint, String tokenValue) throws Throwable {
+        // 获取 Redis 中缓存的值
+        String redisValue = stringRedisTemplate.opsForValue().get(tokenValue);
+        if (redisValue == null) {
+            // Redis 中无值
+
+            // 执行方法
+            Object proceed = joinPoint.proceed();
+            // 将结果放入 Redis 中
+            stringRedisTemplate.opsForValue().set(tokenValue, JSON.toJSONString(proceed));
+            // 返回执行结果
+            return proceed;
+        } else {
+            // 返回 Redis 中的结果
+            return JSON.parseObject(redisValue);
+        }
     }
 
 }
