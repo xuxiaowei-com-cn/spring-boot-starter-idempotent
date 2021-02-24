797d9f6dcf50a8904475e0f291c090288e50771b
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-02-24 12:51:31 +0800

    幂等状态 枚举

---
+++ src/main/java/cn/com/xuxiaowei/boot/idempotent/context/StatusEnum.java
@@ -0,0 +1,41 @@
+package cn.com.xuxiaowei.boot.idempotent.context;
+
+/**
+ * 幂等状态 枚举
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+public enum StatusEnum {
+
+    /**
+     * 未创建
+     */
+    NOT_CREATE,
+
+    /**
+     * 创建
+     */
+    CREATE,
+
+    /**
+     * 正在执行
+     */
+    EXECUTE,
+
+    /**
+     * 正常
+     */
+    NORMAL,
+
+    /**
+     * 重复执行
+     */
+    REPEAT,
+
+    /**
+     * 过期
+     */
+    EXPIRE
+
+}
