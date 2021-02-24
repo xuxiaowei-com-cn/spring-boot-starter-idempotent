5c651b282c290b5e3d4bea70c6a3fbcf6404dd12
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-02-24 12:56:35 +0800

    幂等内容

--- src/main/java/cn/com/xuxiaowei/boot/idempotent/context/IdempotentContext.java
+++ src/main/java/cn/com/xuxiaowei/boot/idempotent/context/IdempotentContext.java
@@ -1,7 +1,9 @@
 package cn.com.xuxiaowei.boot.idempotent.context;
 
-import java.util.HashMap;
-import java.util.Map;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
 
 /**
  * 幂等内容
@@ -9,20 +11,50 @@ import java.util.Map;
  * @author xuxiaowei
  * @since 0.0.1
  */
-public class IdempotentContext {
+@Data
+public class IdempotentContext implements Serializable {
 
-    private final Map<String, String> MAP = new HashMap<>();
+    private static final long serialVersionUID = 1L;
 
-    public String put(String key, String value) {
-        return MAP.put(key, value);
-    }
+    /**
+     * 幂等 Token
+     */
+    private String token;
 
-    public String remove(String key) {
-        return MAP.remove(key);
-    }
+    /**
+     * 幂等状态
+     */
+    private StatusEnum status;
 
-    public String get(String key) {
-        return MAP.get(key);
-    }
+    /**
+     * 幂等Token创建时间
+     */
+    private LocalDateTime tokenDate;
+
+    /**
+     * 请求时间（首次）
+     */
+    private LocalDateTime requestDate;
+
+    /**
+     * 执行结果时间（首次）
+     */
+    private LocalDateTime resultDate;
+
+    /**
+     * 过期时间
+     */
+    private LocalDateTime expireDate;
+
+    /**
+     * 调用次数
+     * <p>
+     * -1：未创建幂等，未找到
+     * -0：已创建，未调用
+     * 1：调用一次
+     * 2：调用两次
+     * 3：调用三次
+     */
+    private Integer number = -1;
 
 }
