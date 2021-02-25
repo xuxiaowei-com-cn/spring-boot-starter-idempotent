cb4fe0ed11f6d2f2e3f1ff07b06968bebc492975
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-02-25 16:56:53 +0800

    幂等 Redis 服务

---
+++ src/main/java/cn/com/xuxiaowei/boot/idempotent/service/IdempotentRedisService.java
@@ -0,0 +1,12 @@
+package cn.com.xuxiaowei.boot.idempotent.service;
+
+/**
+ * 幂等 Redis 服务
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+public interface IdempotentRedisService {
+
+
+}
---
+++ src/main/java/cn/com/xuxiaowei/boot/idempotent/service/impl/RedisServiceImpl.java
@@ -0,0 +1,16 @@
+package cn.com.xuxiaowei.boot.idempotent.service.impl;
+
+import cn.com.xuxiaowei.boot.idempotent.service.IdempotentRedisService;
+import org.springframework.stereotype.Service;
+
+/**
+ * 幂等 Redis 服务 实现类
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@Service
+public class RedisServiceImpl implements IdempotentRedisService {
+
+
+}
