5ec2e9313b4906cbc48d92bfa1a422bb5969be5c
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-02-25 17:02:28 +0800

    Update IdempotentRedisService.java

--- src/main/java/cn/com/xuxiaowei/boot/idempotent/service/IdempotentRedisService.java
+++ src/main/java/cn/com/xuxiaowei/boot/idempotent/service/IdempotentRedisService.java
@@ -3,10 +3,26 @@ package cn.com.xuxiaowei.boot.idempotent.service;
 /**
  * 幂等 Redis 服务
  *
+ * @param <T> Redis数据类型
  * @author xuxiaowei
  * @since 0.0.1
  */
-public interface IdempotentRedisService {
+public interface IdempotentRedisService<T> {
 
+    /**
+     * 根据 Redis Key 获取 Redis数据
+     *
+     * @param key Redis Key
+     * @return Redis数据
+     */
+    T get(String key);
+
+    /**
+     * 将数据放入Redis中
+     *
+     * @param data Redis数据
+     * @return 返回保存结果
+     */
+    boolean save(T data);
 
 }
