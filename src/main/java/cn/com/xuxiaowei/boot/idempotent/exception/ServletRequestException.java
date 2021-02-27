5b309a1a710e770c0266ee7321a2dd1e941c3b37
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-02-27 21:54:46 +0800

    Servlet 请求异常

---
+++ src/main/java/cn/com/xuxiaowei/boot/idempotent/exception/ServletRequestException.java
@@ -0,0 +1,15 @@
+package cn.com.xuxiaowei.boot.idempotent.exception;
+
+/**
+ * Servlet 请求异常
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+public class ServletRequestException extends IdempotentException {
+
+    public ServletRequestException(String message) {
+        super(message);
+    }
+
+}
