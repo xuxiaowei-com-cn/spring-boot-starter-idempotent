753a24fa98fd121e0dadaf7e59b3b681373e3e39
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-02-28 20:56:50 +0800

    Update FutureTaskUtils.java

--- src/main/java/cn/com/xuxiaowei/boot/idempotent/util/FutureTaskUtils.java
+++ src/main/java/cn/com/xuxiaowei/boot/idempotent/util/FutureTaskUtils.java
@@ -2,8 +2,6 @@ package cn.com.xuxiaowei.boot.idempotent.util;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
 /**
@@ -20,15 +18,14 @@ public class FutureTaskUtils {
      * @param joinPoint 环绕通知 切面方法信息
      * @return 返回 执行结果
      */
-    public static Object proceed(ProceedingJoinPoint joinPoint) throws ExecutionException, InterruptedException {
-        FutureTask<?> futureTask = new FutureTask<>((Callable<?>) () -> {
+    public static FutureTask<Object> proceed(ProceedingJoinPoint joinPoint) {
+        return new FutureTask<>(() -> {
             try {
                 return joinPoint.proceed();
             } catch (Throwable throwable) {
                 throw new Exception(throwable);
             }
         });
-        return futureTask.get();
     }
 
 }
