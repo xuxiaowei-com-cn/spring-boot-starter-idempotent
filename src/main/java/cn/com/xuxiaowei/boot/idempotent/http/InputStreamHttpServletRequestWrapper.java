c05d09336da61a5d39a3ddf0a2948eb8750a8c59
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-01-24 13:59:27 +0800

    请求流支持多次获取

--- src/main/java/cn/com/xuxiaowei/boot/idempotent/http/InputStreamHttpServletRequestWrapper.java
+++ src/main/java/cn/com/xuxiaowei/boot/idempotent/http/InputStreamHttpServletRequestWrapper.java
@@ -1,61 +1,89 @@
 package cn.com.xuxiaowei.boot.idempotent.http;
 
-import cn.com.xuxiaowei.boot.idempotent.util.RequestUtils;
+import org.apache.commons.io.IOUtils;
 
 import javax.servlet.ReadListener;
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
+import java.io.*;
 
 /**
- * 请求流支持多次获取{@link HttpServletRequest}
+ * 请求流支持多次获取 {@link HttpServletRequest}
  *
  * @author xuxiaowei
+ * @see <a href="https://stackoverflow.com/questions/10210645/http-servlet-request-lose-params-from-post-body-after-read-it-once/17129256#17129256">http-servlet-request-lose-params-from-post-body-after-read-it-once</a>
  * @since 0.0.1
  */
 public class InputStreamHttpServletRequestWrapper extends HttpServletRequestWrapper {
 
-    private final byte[] body;
-
     /**
-     * 构造包装给定请求的请求对象。
-     *
-     * @param request 包装请求
-     * @throws IOException 读取流异常
+     * 用于缓存输入流
      */
-    public InputStreamHttpServletRequestWrapper(HttpServletRequest request) throws IOException {
+    private ByteArrayOutputStream cachedBytes;
+
+    public InputStreamHttpServletRequestWrapper(HttpServletRequest request) {
         super(request);
-        String characterEncoding = request.getCharacterEncoding();
-        String inputStream = RequestUtils.getInputStream(request);
-        body = inputStream == null ? null : inputStream.getBytes(characterEncoding);
     }
 
     @Override
     public ServletInputStream getInputStream() throws IOException {
-        final ByteArrayInputStream inputStream = new ByteArrayInputStream(body);
-        return new ServletInputStream() {
-            @Override
-            public boolean isFinished() {
-                return false;
-            }
+        if (cachedBytes == null) {
+            // 首次获取流时，将流放入 缓存输入流 中
+            cacheInputStream();
+        }
 
-            @Override
-            public boolean isReady() {
-                return false;
-            }
+        // 从 缓存输入流 中获取流并返回
+        return new CachedServletInputStream(cachedBytes.toByteArray());
+    }
 
-            @Override
-            public void setReadListener(ReadListener listener) {
+    @Override
+    public BufferedReader getReader() throws IOException {
+        return new BufferedReader(new InputStreamReader(getInputStream()));
+    }
 
-            }
+    /**
+     * 首次获取流时，将流放入 缓存输入流 中
+     */
+    private void cacheInputStream() throws IOException {
+        // 缓存输入流以便多次读取。为了方便, 我使用 org.apache.commons IOUtils
+        cachedBytes = new ByteArrayOutputStream();
+        IOUtils.copy(super.getInputStream(), cachedBytes);
+    }
 
-            @Override
-            public int read() throws IOException {
-                return inputStream.read();
-            }
-        };
+    /**
+     * 读取缓存的请求正文的输入流
+     * <p>
+     * 用于根据 缓存输入流 创建一个可返回的 {@link ServletInputStream}
+     */
+    public static class CachedServletInputStream extends ServletInputStream {
+
+        private final ByteArrayInputStream input;
+
+        public CachedServletInputStream(byte[] buf) {
+            // 从缓存的请求正文创建一个新的输入流
+            input = new ByteArrayInputStream(buf);
+        }
+
+        @Override
+        public boolean isFinished() {
+            return false;
+        }
+
+        @Override
+        public boolean isReady() {
+            return false;
+        }
+
+        @Override
+        public void setReadListener(ReadListener listener) {
+
+        }
+
+        @Override
+        public int read() throws IOException {
+            return input.read();
+        }
     }
 
 }
