bfd0283654aa307f34ed0203a55cbd27dff7fd3d
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-10-18 09:06:39 +0800

    :memo: 修正注释

--- src/main/java/cn/com/xuxiaowei/boot/next/idempotent/service/TimeoutExceptionService.java
+++ src/main/java/cn/com/xuxiaowei/boot/next/idempotent/service/TimeoutExceptionService.java
@@ -21,7 +21,7 @@ public interface TimeoutExceptionService {
 	 * 1、如果 {@link Controller} 返回数据类型与本方法返回数据类型相同，可直接使用 `return` 返回结果
 	 * <p>
 	 * 2、如果 {@link Controller} 返回数据类型与本方法返回数据类型不同，可直接使用 `return` 返回 `null`， 使用
-	 * `javax.servlet.http.HttpServletResponse` 返回结果
+	 * `jakarta.servlet.http.HttpServletResponse` 返回结果
 	 * @param request 请求
 	 * @param response 响应
 	 * @param joinPoint 切点
